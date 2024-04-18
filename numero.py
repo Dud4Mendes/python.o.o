06f625e0e06691585e4177276b4826a345f3bbf7
Dud4Mendes <129318829+Dud4Mendes@users.noreply.github.com>
2024-04-18 11:47:42 -0300

    Add files via upload

---
+++ numero.py
@@ -0,0 +1,21 @@
+num1 = int(input("digite o primeiro numero"))
+num2 = int(input("digite o segndo numero"))
+operacao = input("qual é a ua operação?")
+
+def adicao(num1, num2):
+    return num1 + num2
+
+def subtracao(num1, num2):
+    return = (num1 - num2)
+
+def divisao(num1, num2):
+    if num2 == 0:
+        return "o numero não pode ser dividido por 0"
+    else num1 / num2    
+    
+
+def multiplicacao(num1, num2):  
+    return = (num1 * num2)   
+   
+if (operacao == '+'):
+    print(somar(num1, num2))
